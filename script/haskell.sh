efdef48db2d30e3b506bde1854c4a69c6eeb8c58
Kenneth Kan <kenhkan@gmail.com>
2015-07-02 07:25:20 -0400

    Link Cabal libraries statically

--- script/haskell.sh
+++ script/haskell.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get install -y cabal-install-1.20 ghc-7.8.4
 
-# Link Cabal and GHC
+# Link Cabal and GHC statically
 CABAL_PATH=/opt/cabal/1.20/bin
 GHC_PATH=/opt/ghc/7.8.4/bin
 sudo ln -s $CABAL_PATH/cabal /usr/bin/cabal
@@ -19,6 +19,12 @@ sudo ln -s $GHC_PATH/hpc /usr/bin/hpc
 sudo ln -s $GHC_PATH/hsc2hs /usr/bin/hsc2hs
 sudo ln -s $GHC_PATH/runghc /usr/bin/runghc
 sudo ln -s $GHC_PATH/runhaskell /usr/bin/runhaskell
+
+# Still link Cabal libraries via PATH
+cat >> /etc/bash.bashrc <<EOF
+export PATH=~/.cabal/bin:$PATH
+EOF
+export PATH=~/.cabal/bin:$PATH
 
 # Additional required libraries
 sudo apt-get install -y zlib1g-dev
