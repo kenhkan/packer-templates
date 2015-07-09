9fb971b7d9376a9ee7889a62d9cf3584ee9b43b3
Kenneth Kan <kenhkan@gmail.com>
2015-07-08 23:28:44 -0400

    Install cabal and yesod as binaries

--- script/stack.sh
+++ script/stack.sh
@@ -11,18 +11,21 @@ wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | s
 echo 'deb http://download.fpcomplete.com/ubuntu/trusty stable main'|sudo tee /etc/apt/sources.list.d/fpco.list
 sudo apt-get update && sudo apt-get install stack -y
 
-# Link ghc executables
-STACK_GHC_PATH=/home/vagrant/.stack/programs/x86_64-linux/ghc-7.8.4/bin
-BIN_PATH=/usr/bin
-sudo ln -s $STACK_GHC_PATH/ghc $BIN_PATH/ghc
-sudo ln -s $STACK_GHC_PATH/ghci $BIN_PATH/ghci
-sudo ln -s $STACK_GHC_PATH/ghc-pkg $BIN_PATH/ghc-pkg
-sudo ln -s $STACK_GHC_PATH/haddock $BIN_PATH/haddock
-sudo ln -s $STACK_GHC_PATH/hp2ps $BIN_PATH/hp2ps
-sudo ln -s $STACK_GHC_PATH/hpc $BIN_PATH/hpc
-sudo ln -s $STACK_GHC_PATH/hsc2hs $BIN_PATH/hsc2hs
-sudo ln -s $STACK_GHC_PATH/runghc $BIN_PATH/runghc
-sudo ln -s $STACK_GHC_PATH/runhaskell $BIN_PATH/runhaskell
+## Link ghc executables
+#STACK_GHC_PATH=/home/vagrant/.stack/programs/x86_64-linux/ghc-7.8.4/bin
+#BIN_PATH=/usr/bin
+#sudo ln -s $STACK_GHC_PATH/ghc $BIN_PATH/ghc
+#sudo ln -s $STACK_GHC_PATH/ghci $BIN_PATH/ghci
+#sudo ln -s $STACK_GHC_PATH/ghc-pkg $BIN_PATH/ghc-pkg
+#sudo ln -s $STACK_GHC_PATH/haddock $BIN_PATH/haddock
+#sudo ln -s $STACK_GHC_PATH/hp2ps $BIN_PATH/hp2ps
+#sudo ln -s $STACK_GHC_PATH/hpc $BIN_PATH/hpc
+#sudo ln -s $STACK_GHC_PATH/hsc2hs $BIN_PATH/hsc2hs
+#sudo ln -s $STACK_GHC_PATH/runghc $BIN_PATH/runghc
+#sudo ln -s $STACK_GHC_PATH/runhaskell $BIN_PATH/runhaskell
+
+# Install command-line binaries
+stack install cabal-install
 
 # Link cabal/stack libraries via PATH
 cat >> ~/.bashrc <<EOF
---
+++ script/yesod.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+stack install yesod-bin-1.4.11
